1083da42047672dfadef04283f7817c7e1104032
Nefedova <Katerina163>
2021-04-13 08:57:11 +0400

    5.2.1. Реализовать SimpleArray<T> [475130]

--- src/main/java/ru/job4j/generics/SimpleArray.java
+++ src/main/java/ru/job4j/generics/SimpleArray.java
@@ -5,43 +5,53 @@ import org.hamcrest.internal.ArrayIterator;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public class SimpleArray<T> implements Iterable {
     private T[] array;
+    private int count = 0;
 
-    public SimpleArray(Class<T> t, int i) {
-        @SuppressWarnings("unchecked")
-        final T[] array = (T[]) Array.newInstance(t, i);
-        this.array = array;
+    public SimpleArray(int i) {
+        this.array = (T[]) new Object[i];
     }
 
     public void add(T model) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == null) {
-                array[i] = model;
-                break;
-            }
-        }
+        array[count++] = model;
     }
 
     public void set(int index, T model) {
-        Objects.checkIndex(index, array.length);
+        Objects.checkIndex(index, count);
         array[index] = model;
     }
 
     public void remove(int index) {
-        Objects.checkIndex(index, array.length);
+        Objects.checkIndex(index, count);
         System.arraycopy(array, index + 1, array, index, array.length - index - 1);
     }
 
     public T get(int index) {
-        Objects.checkIndex(index, array.length);
+        Objects.checkIndex(index, count);
         return array[index];
     }
 
     @Override
     public Iterator iterator() {
-        return Arrays.asList(array).iterator();
+        return new Iterator<T>() {
+            private int point = 0;
+
+            @Override
+            public boolean hasNext() {
+                return point < count;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return array[point++];
+            }
+        };
     }
 }
--- src/test/java/ru/job4j/generics/SimpleArrayTest.java
+++ src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 public class SimpleArrayTest {
     @Test
     public void addInt() {
-        SimpleArray<Integer> ex = new SimpleArray<>(Integer.class, 2);
+        SimpleArray<Integer> ex = new SimpleArray<>(2);
         ex.add(1);
         ex.add(2);
         assertThat(1, is(ex.get(0)));
@@ -17,7 +17,7 @@ public class SimpleArrayTest {
 
     @Test
     public void addString() {
-        SimpleArray<String> ex = new SimpleArray<>(String.class, 2);
+        SimpleArray<String> ex = new SimpleArray<>(2);
         ex.add("1");
         ex.add("2");
         assertThat("1", is(ex.get(0)));
@@ -26,14 +26,14 @@ public class SimpleArrayTest {
 
     @Test
     public void addNull() {
-        SimpleArray<String> ex = new SimpleArray<>(String.class, 2);
+        SimpleArray<String> ex = new SimpleArray<>(2);
         ex.add(null);
         assertNull(ex.get(0));
     }
 
     @Test
     public void setInt() {
-        SimpleArray<Integer> ex = new SimpleArray<>(Integer.class, 2);
+        SimpleArray<Integer> ex = new SimpleArray<>(2);
         ex.add(1);
         ex.set(0, 2);
         assertThat(2, is(ex.get(0)));
@@ -41,7 +41,7 @@ public class SimpleArrayTest {
 
     @Test
     public void removeInt() {
-        SimpleArray<Integer> ex = new SimpleArray<>(Integer.class, 4);
+        SimpleArray<Integer> ex = new SimpleArray<>(4);
         ex.add(1);
         ex.add(2);
         ex.add(3);
