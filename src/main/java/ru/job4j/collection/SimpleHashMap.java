8536130a5f31eb9e6b50ef0e44290e41602758d0
Nefedova <Katerina163>
2021-04-19 12:03:28 +0400

    8. Реализовать собственную структуру данных - HashMap [475130]

--- src/main/java/ru/job4j/collection/SimpleHashMap.java
+++ src/main/java/ru/job4j/collection/SimpleHashMap.java
@@ -4,123 +4,135 @@ import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class SimpleHashMap<K, V> implements Map<K, V> {
-    private Object[] container;
-    private Object[] keyContainer;
-    private int count = 0;
-    private int modCount = 0;
+public class SimpleHashMap<K, V> implements Iterable {
+    private MapEntry<K, V> table;
 
     public SimpleHashMap() {
-        this.container = new Object[2];
-        this.keyContainer = new Object[2];
+        this.table = new MapEntry();
     }
 
-    @Override
     public boolean insert(K key, V value) {
-        if (count == container.length) {
-            increase();
-        }
-        if (key == null) {
-            if (container[0] != null && container[0] != value) {
-                return false;
-            }
-            container[0] = value;
-        } else {
-            int i = key.hashCode() & (container.length - 1);
-            if (container[i] != null && container[i] != value) {
-                return false;
-            }
-            container[i] = value;
-            keyContainer[i] = key;
-        }
-        count++;
-        modCount++;
-        return true;
+        return table.insert(key, value);
     }
 
-    private void increase() {
-        Object[] array = new Object[container.length * 2];
-        Object[] keyArray = new Object[keyContainer.length * 2];
-        if (keyContainer[0] != null) {
-            array[0] = container[0];
-        }
-        for (int index = 1; index < keyContainer.length; index++) {
-            if (keyContainer[index] != null) {
-                int i = keyContainer[index].hashCode() & (array.length - 1);
-                array[i] = container[index];
-                keyArray[i] = keyContainer[index];
-            }
-        }
-        container = array;
-        keyContainer = keyArray;
-    }
-
-    @Override
     public V get(K key) {
-        if (key == null) {
-            return (V) container[0];
-        }
-        int i = key.hashCode() & (container.length - 1);
-        return (V) container[i];
+        return table.get(key);
+    }
+
+    public boolean delete(K key) {
+        return table.delete(key);
     }
 
     @Override
-    public boolean delete(K key) {
-        count--;
-        modCount++;
-        if (key == null) {
-            container[0] = null;
+    public Iterator<V> iterator() {
+        return table.iterator();
+    }
+
+    private static class MapEntry<K, V> implements Map<K, V>, Iterable<V> {
+        private final double LOAD_FACTOR = 0.75;
+        private Object[] container;
+        private Object[] keyContainer;
+        private int count = 0;
+        private int modCount = 0;
+
+        public MapEntry() {
+            this.container = new Object[2];
+            this.keyContainer = new Object[2];
+        }
+
+        private void increase() {
+            Object[] array = new Object[container.length * 2];
+            Object[] keyArray = new Object[keyContainer.length * 2];
+            if (keyContainer[0] != null) {
+                array[0] = container[0];
+            }
+            for (int index = 1; index < keyContainer.length; index++) {
+                if (keyContainer[index] != null) {
+                    int i = keyContainer[index].hashCode() & (array.length - 1);
+                    array[i] = container[index];
+                    keyArray[i] = keyContainer[index];
+                }
+            }
+            container = array;
+            keyContainer = keyArray;
+        }
+
+        @Override
+        public boolean insert(K key, V value) {
+            if (count == container.length * LOAD_FACTOR) {
+                increase();
+            }
+            if (key == null) {
+                if (container[0] != null && container[0] != value) {
+                    return false;
+                }
+                container[0] = value;
+            } else {
+                int i = key.hashCode() & (container.length - 1);
+                if (container[i] != null && container[i] != value) {
+                    return false;
+                }
+                container[i] = value;
+                keyContainer[i] = key;
+            }
+            count++;
+            modCount++;
             return true;
         }
-        int i = key.hashCode() & (container.length - 1);
-        container[i] = null;
-        keyContainer[i] = null;
-        return true;
-    }
 
-    public Iterator<V> iteratorValue() {
-        return new Iterator<V>() {
-            private int point = 0;
-            private final int expectedModCount = modCount;
-
-            @Override
-            public boolean hasNext() {
-                return point < count;
+        @Override
+        public V get(K key) {
+            if (key == null) {
+                return (V) container[0];
             }
-
-            @Override
-            public V next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
-                return (V) container[point++];
+            int i = key.hashCode() & (container.length - 1);
+            if (keyContainer[i] == null || !keyContainer[i].equals(key)) {
+                return null;
             }
-        };
-    }
+            return (V) container[i];
+        }
 
-    public Iterator<K> iteratorKey() {
-        return new Iterator<K>() {
-            private int point = 0;
-            private final int expectedModCount = modCount;
-
-            @Override
-            public boolean hasNext() {
-                return point < count;
+        @Override
+        public boolean delete(K key) {
+            if (key == null) {
+                container[0] = null;
+                count--;
+                modCount++;
+                return true;
             }
-
-            @Override
-            public K next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
-                return (K) keyContainer[point++];
+            int i = key.hashCode() & (container.length - 1);
+            if (!keyContainer[i].equals(key)) {
+                return false;
             }
-        };
+            container[i] = null;
+            keyContainer[i] = null;
+            count--;
+            modCount++;
+            return true;
+        }
+
+        @Override
+        public Iterator<V> iterator() {
+            return new Iterator<V>() {
+                private int point = 0;
+                private final int expectedModCount = modCount;
+
+                @Override
+                public boolean hasNext() {
+                    return point < count;
+                }
+
+                @Override
+                public V next() {
+                    if (!hasNext()) {
+                        throw new NoSuchElementException();
+                    }
+                    if (expectedModCount != modCount) {
+                        throw new ConcurrentModificationException();
+                    }
+                    return (V) container[point++];
+                }
+            };
+        }
     }
 }
--- src/test/java/ru/job4j/collection/SimpleHashMapTest.java
+++ src/test/java/ru/job4j/collection/SimpleHashMapTest.java
@@ -25,28 +25,28 @@ public class SimpleHashMapTest {
         ex.insert(1, 1);
         ex.insert(2, 2);
         ex.delete(1);
-        assertThat(null, is(ex.get(1)));
+        assertNull(ex.get(1));
     }
 
     @Test
     public void whenAddThenIt() {
         SimpleHashMap<Integer, String> ex = new SimpleHashMap<>();
         ex.insert(null, "first");
-        String rsl = ex.iteratorValue().next();
+        String rsl = ex.iterator().next();
         assertThat(rsl, is("first"));
     }
 
     @Test(expected = NoSuchElementException.class)
     public void whenGetEmptyFromIt() {
         SimpleHashMap<Integer, Integer> ex = new SimpleHashMap<>();
-        ex.iteratorKey().next();
+        ex.iterator().next();
     }
 
     @Test(expected = ConcurrentModificationException.class)
     public void whenCorruptedIt() {
         SimpleHashMap<Integer, String> ex = new SimpleHashMap<>();
         ex.insert(1, "first");
-        Iterator<String> it = ex.iteratorValue();
+        Iterator<String> it = ex.iterator();
         ex.insert(2, "second");
         it.next();
     }
