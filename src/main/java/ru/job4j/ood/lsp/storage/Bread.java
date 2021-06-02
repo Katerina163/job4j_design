9da6b4c4ea1bb05f4340f14e69dce6d89ec0cf19
Nefedova <Katerina163>
2021-06-02 14:49:19 +0400

    CheckStyle

--- src/main/java/ru/job4j/ood/lsp/storage/Bread.java
+++ src/main/java/ru/job4j/ood/lsp/storage/Bread.java
@@ -1,10 +1,10 @@
 package ru.job4j.ood.lsp.storage;
 
 import java.time.LocalDate;
-
-public class Bread extends Food {
-
-    public Bread(String name, LocalDate expiryDate, LocalDate createDate, double price) {
-        super(name, expiryDate, createDate, price);
-    }
-}
+//
+//public class Bread extends Food {
+//
+//    public Bread(String name, LocalDate expiryDate, LocalDate createDate, double price) {
+//        super(name, expiryDate, createDate, price);
+//    }
+//}
