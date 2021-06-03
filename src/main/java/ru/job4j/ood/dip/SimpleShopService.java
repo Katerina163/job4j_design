3ded9f58a20a152819141f7d7916767e4c46a915
Nefedova <Katerina163>
2021-06-03 10:21:35 +0400

    0. Видео [475130]

---
+++ src/main/java/ru/job4j/ood/dip/SimpleShopService.java
@@ -0,0 +1,69 @@
+package ru.job4j.ood.dip;
+
+import org.apache.log4j.Logger;
+
+import java.util.Optional;
+import java.util.Set;
+
+public class SimpleShopService {
+    private ShopStore shopStore;
+    private OrderService orderService;
+    private static final Logger LOGGER = Logger.getLogger("Shop logger");
+
+    public SimpleShopService(ShopStore shopStore, OrderService orderService) {
+        this.shopStore = shopStore;
+        this.orderService = orderService;
+    }
+
+    public boolean createBucket(User user) {
+        return shopStore.saveUser(user);
+    }
+
+    public boolean createOrder(User user, Order order) {
+        return shopStore.saveOrder(user, order);
+    }
+
+    public boolean addProduct(User user, Order order, Product product) {
+        Optional<Order> orderDB = findOrder(user, order);
+        if (orderDB.isEmpty()) {
+            return false;
+        }
+        return orderDB.get().add(product);
+    }
+
+    private Optional<Order> findOrder(User user, Order order) {
+        return shopStore.getOrders(user).stream()
+                .filter(o -> o.getId() == order.getId()).findFirst();
+    }
+
+    public boolean removeProduct(User user, Order order, Product product) {
+        Optional<Order> orderDB = findOrder(user, order);
+        if (orderDB.isEmpty()) {
+            return false;
+        }
+        return orderDB.get().remove(product.getId());
+    }
+
+    public boolean removeOrder(User user, Order order) {
+        Set<Order> orders = shopStore.getOrders(user);
+        if (orders == null) {
+            return false;
+        }
+        return orders.remove(order);
+    }
+
+    public Check payOrder(User user, Order order) {
+        Optional<Order> orderDB = findOrder(user, order);
+        if (orderDB.isEmpty()) {
+            LOGGER.error("Get error with " + user + " " + order,
+                    new IllegalArgumentException("Invalid order"));
+        }
+        if (orderDB.get().isPayed()) {
+            LOGGER.debug("Get error with "  + user + " " + order,
+                    new IllegalArgumentException("Already payed!"));
+
+        }
+        orderDB.get().setPayed(true);
+        return new Check((int) (System.currentTimeMillis() % 1000_000), "Payed: " + orderDB.get().getId());
+    }
+}
