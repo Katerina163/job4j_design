1cdf0f910a5dcb2ee080f4c0f6006010775e6a03
Nefedova <Katerina163>
2021-06-07 09:47:13 +0400

    CheckStyle

--- src/main/java/ru/job4j/ood/dip/Product.java
+++ src/main/java/ru/job4j/ood/dip/Product.java
@@ -1,7 +1,7 @@
 package ru.job4j.ood.dip;
-
-public class Product extends BaseEntity {
-    public Product(int id, String name) {
-        super(id, name);
-    }
-}
+//
+//public class Product extends BaseEntity {
+//    public Product(int id, String name) {
+//        super(id, name);
+//    }
+//}
